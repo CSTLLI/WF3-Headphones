f50168776a487beead7f5453b9df14d7016fd152
Castelli Valerio <valeriocastellipro@gmail.com>
2022-01-23 15:55:35 +0100

    add js in the project

---
+++ script.js
@@ -0,0 +1,43 @@
+/* ************************************************************************************** */
+/*                                                                                        */																					  
+/* Project: Headphones                                  / $$      /$$ /$$$$$$$$ /$$$$$$   */
+/*                  			                        | $$  /$ | $$| $$_____//$$__  $$  */
+/* script.js                                     	    | $$ /$$$| $$| $$     |__/  \ $$  */
+/*                                                  	| $$/$$ $$ $$| $$$$$     /$$$$$/  */
+/* By: vcastell <valeriocastellipro@gmail.com>	        | $$$$_  $$$$| $$__/    |___  $$  */
+/*                                              		| $$$/ \  $$$| $$      /$$  \ $$  */
+/* Created: 2022/01/22 14:58:01 vcastell     	        | $$/   \  $$| $$     |  $$$$$$/  */
+/* Updated: 2022/01/20 15:53:42 vcastell                |__/     \__/|__/      \______/   */
+/*                                                                    				      */
+/******************************************************************************************/
+
+document.querySelectorAll('.option').forEach(option => {
+	option.addEventListener('click', changeStyle);
+})
+
+function changeStyle() {
+	document.querySelectorAll('.option').forEach(option => {
+		option.classList.remove('active');
+	})
+
+	let num = this.dataset.option;
+
+    document.querySelector('.color-option').src = `images/headphones/color-option0${num}.jpg`;
+	document.querySelector('.color-option').alt = `Option0${num}`;
+
+	if (num == 1){
+		document.querySelector('.product-title').innerHTML = `Rose Gold`;
+	}else if( num == 2){
+		document.querySelector('.product-title').innerHTML = `Gray`;
+	}else if( num == 3){
+		document.querySelector('.product-title').innerHTML = `Gold`;
+	}else if( num == 4){
+		document.querySelector('.product-title').innerHTML = `Blue Gold Red`;
+	}else if( num == 5){
+		document.querySelector('.product-title').innerHTML = `Gray Red Blue`;
+	}else if( num == 6){
+		document.querySelector('.product-title').innerHTML = `Red Blue Gold`;
+	}
+
+	this.classList.add('active');	
+}
